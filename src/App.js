fe843c6289dda8ced2e8796b27f06319a1a1eb56
DARKENFaze <otashblogs@gmail.com>
2024-10-11 17:23:49 +0500

    app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="./pages/Product.js" element={<About />} />
+          <Route path="./pages/Product-Card.js" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => <h2>Home Page</h2>;
+const About = () => <h2>About Page</h2>;
+const Contact = () => <h2>Contact Page</h2>;
 
 export default App;
